d9c9abfd21ebce42cc9052dcf2b169bdc1e4f98a
Ben Fok <benfok8@gmail.com>
2022-02-24 22:34:24 -0800

    some tweaks to html code and css. Added functionality to generate the index.html file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const inquirer = require('inquirer');
 const { managerPrompts, employeePrompts } = require('./lib/prompts');
-const { html, contactCard } = require('./lib/createHtml');
+const { pageHtml, contactCard } = require('./lib/createHtml');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const fs = require('fs');
 
 // array to collect employees entered
 let companyName;
@@ -19,7 +20,7 @@ const startQuestions = () => {
             companyName = company;
             // create new manager object and push to employees array
             const teamManager = new Manager(managerName, parseInt(managerId), managerEmail, parseInt(officeNumber));
-            const html = contactCard(teamManager.getRole(), teamManager.getName(), teamManager.getId(), teamManager.getEmail(), `<i class="fa-solid fa-clipboard-list-check"></i>`, 'Office Number: ', teamManager.officeNumber)
+            const html = contactCard(teamManager.getRole(), teamManager.getName(), teamManager.getId(), teamManager.getEmail(), `<i class="fa-solid fa-clipboard-list"></i>`, 'Office Number: ', teamManager.officeNumber)
             employeeCardsHtml += html;
             moreQuestions();
          })
@@ -32,10 +33,13 @@ const moreQuestions = () => {
     inquirer.prompt(employeePrompts)
        .then(({ addMore, name, id, email, github, school }) => {
             if(addMore === 'done') {
-                console.log(employeeCardsHtml);
+                console.log('Thanks for entering your team information!')
+                const str = pageHtml(companyName, employeeCardsHtml);
+                createIndexHtml(str);
+                return;
             } else if (addMore === 'Engineer') {
                         const newEngineer = new Engineer(name, parseInt(id), email, github);
-                        const html = contactCard(newEngineer.getRole(), newEngineer.getName(), newEngineer.getId(), newEngineer.getEmail(), `<i class="fa-solid fa-square-terminal"></i>`, 'GitHub Profile: ', newEngineer.getGithub())
+                        const html = contactCard(newEngineer.getRole(), newEngineer.getName(), newEngineer.getId(), newEngineer.getEmail(), `<i class="fa-solid fa-code-branch"></i>`, 'GitHub Profile: ', `<a href="https://github.com/${newEngineer.getGithub()}" target="_blank">${newEngineer.getGithub()}</a>`)
                         employeeCardsHtml += html;
                             } else { 
                             const newIntern = new Intern(name, parseInt(id), email, school);
@@ -52,41 +56,11 @@ const moreQuestions = () => {
 startQuestions();
 
 
-
-
-
-
-// prior code
-
-// const createCards = (employeesArray) => {
-//     employeesArray.forEach(({role, name: employeeName, id, email}) => {
-//         const info = collectRoleData(role);
-//         employeeCardsHtml.push(contactCard(role, employeeName, id, email, info.icon, info.roleKey)); // need to add keyValue somehow
-//         console.log(employeeCardsHtml);
-//     })
-// }
-
-// const collectRoleData = role => {
-//     for (i = 0; i < roleData.length; i++) {
-//         if (roleData[i].includes(role)) {
-//             const icon = roleData[i][1];
-//             const roleKey = roleData[i][2];
-//             return { icon, roleKey };
-//         }
-//     }
-// }
-
-
-
-        // // if the user prompted to add another employee, push the current answers to the array then restart the questions
-        // if(answers.restart) {
-        //     employees.push(answers);
-        //     runQuestions();
-        // } else {
-        // // else push the current answers to the array and run the code to create the HTML
-        //     employees.push(answers);
-        //     console.log('Thank you for entering your employees');
-        //     console.log(employees);
-        //     createCards(employees);
-        //     // run code to create file
-        // }
+const createIndexHtml = (str) => {
+    fs.writeFile('./dist/index.html', str, (error) => {
+        if (error) {
+            console.log(error);
+        }
+        console.log('\nTeam profile created!');
+    });
+};
